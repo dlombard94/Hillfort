3fe5df85d5901646007d0e3acdbadd1b48dd3095
DLOMBARD\Darragh <112500343@umail.ucc.ie>
2020-01-02 18:52:35 +0000

    added hillfort model

---
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -0,0 +1,31 @@
+package org.wit.hillfort.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hillfort.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.wit.hillfort.R
+import org.wit.hillfort.models.HillfortModel
+
+class HillfortActivity : AppCompatActivity(), AnkoLogger {
+
+    var hillfort = HillfortModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hillfort)
+        info("Hillfort Activity started..")
+
+        btnAdd.setOnClickListener() {
+            hillfort.title = hillfortTitle.text.toString()
+            if (hillfort.title.isNotEmpty()) {
+                info("add Button Pressed: $hillfortTitle")
+            }
+            else {
+                toast ("Please Enter a title")
+            }
+        }
+    }
+}
